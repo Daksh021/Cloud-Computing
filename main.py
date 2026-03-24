a74b30df623d509cd1f30b1b7b66125a73ba9687
Daksh021 <tiwarydaksh21@gmail.com>
2026-03-24 15:18:38 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname()
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
